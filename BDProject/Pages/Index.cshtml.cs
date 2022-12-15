e753d15b33d9ed309cb34b87e0a06c0fdd69142a
BenOthmanZied <benothmanzied6@gmail.com>
2022-12-16 00:53:42 +0100

    first commit

---
+++ BDProject/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ML;
+
+namespace BDProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PredictionEnginePool<MLModel.ModelInput, MLModel.ModelOutput> _predictionEnginePool;
+
+        public IndexModel(PredictionEnginePool<MLModel.ModelInput, MLModel.ModelOutput> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnGetAnalyzeSentiment([FromQuery] string text)
+        {
+            if (String.IsNullOrEmpty(text)) return Content("Neutral");
+            var input = new MLModel.ModelInput { Col0 = text };
+            var prediction = _predictionEnginePool.Predict(input);
+            float x = prediction.PredictedLabel;
+            var sentiment = Convert.ToBoolean(x) ? "Not Toxic" : "Toxic";
+            return Content(sentiment);
+
+
+
+
+        }
+    }
+}
